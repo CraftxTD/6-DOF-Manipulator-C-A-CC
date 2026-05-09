6a52dab80edb0d9c48f2dc6f38ef44ba43c39a12
CraftxTD <craftxtd@gmail.com>
2026-05-09 15:07:50 -0400

    new

--- startup.lua
+++ startup.lua
@@ -1,6 +1,6 @@
 -- NOTE: Link: wget https://raw.githubusercontent.com/CraftxTD/6-DOF-Manipulator-C-A-CC/refs/heads/main/startup.lua  /startup.lua
 local directories = {
-	programs = { "controller.lua" },
+	programs = { "controller.lua", "gyro.lua" },
 	protocols = { "calculate.lua", "channels.lua", "geometry.lua", "network.lua" },
 	ship = { "dock.lua", "pivot.lua" },
 	test = { "test.lua" },
